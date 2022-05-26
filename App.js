fc9e19663bf1561ddd0f2899e48f4673fab40a65
Laura del Toro <laura.deltoro@seddi.com>
2022-05-26 19:36:57 +0100

    Add routing and ticket page

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
+import 'react-native-gesture-handler';
 import React from 'react';
-import {StatusBar} from 'react-native';
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import Home from './Home';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle={'dark-content'} hidden />
-      <Home username="Sports Fan" />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home" headerMode="screen">
+          <Stack.Screen name="Home" options={{ headerShown: false }}>
+            {props => <Home userName="Sports Fan" {...props} />}
+          </Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 };
