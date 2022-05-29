b404f2442c434a2730693f7c518831442effe58b
Laura del Toro <laura.deltoro@seddi.com>
2022-05-29 11:59:19 +0100

    Fix prettier issue

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StatusBar } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import {StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 import Home from './Home';
-import { createStackNavigator } from '@react-navigation/stack';
+import {createStackNavigator} from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
@@ -13,7 +13,7 @@ const App = () => {
       <StatusBar barStyle={'dark-content'} hidden />
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Home" headerMode="screen">
-          <Stack.Screen name="Home" options={{ headerShown: false }}>
+          <Stack.Screen name="Home" options={{headerShown: false}}>
             {props => <Home userName="Sports Fan" {...props} />}
           </Stack.Screen>
         </Stack.Navigator>
